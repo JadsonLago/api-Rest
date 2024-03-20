0391d90d0c48db8195d30e8a8507ca627a8a04ec
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-19 21:57:29 -0300

    mudularização do servidor express

---
+++ server.js
@@ -0,0 +1,10 @@
+import app from "./src/app.js"
+
+// definir a porta
+const port = 3000
+
+// escutar a porta
+app.listen(port, () => {
+    console.log(`Example app listening on  port http://localhost:${port}`)
+  })
+
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
+
 
 // Rota GET padrão
 app.get('/', (req, res) => {
@@ -11,6 +11,6 @@ app.get('/curso', (req, res) => {
     res.send('Este é o curso de NodeJs!')
   })
 
-app.listen(port, () => {
-  console.log(`Example app listening on  port http://localhost:${port}`)
-})
+export default app  
+
+
