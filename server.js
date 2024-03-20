06832248c3307fcad0f1b799894d7d814be7025b
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-19 22:20:32 -0300

    criacao de mock selecoes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import app from "./src/app.js"
 
 // definir a porta
-const port = 3000
+const PORT = 3000
 
 // escutar a porta
-app.listen(port, () => {
-    console.log(`Example app listening on  port http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on  port http://localhost:${PORT}`)
   })
 
--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
+// Mock
+const selecoes = [
+{id: 1, selecao: "Brasil", grupo: "G"},
+{id: 2, selecao: "Suica", grupo: "G"},
+{id: 3, selecao: "Sérvia", grupo: "G"},
+{id: 5, selecao: "Camarões", grupo: "G"}
+]
 
 // Rota GET padrão
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/curso', (req, res) => {
-    res.send('Este é o curso de NodeJs!')
-  })
+app.get("/curso", (req, res) => {
+  res.send("Este é o curso de NodeJs!");
+});
 
-export default app  
+app.get("/selecoes", (req, res) => {
+  res.status(200).send(selecoes);
+});
 
-
+export default app;
